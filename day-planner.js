0af96e83ad1efad8ace823670973865691104f79
Nathan Schmitzer <nathan.schmitzer@yahoo.com>
2020-08-20 15:03:28 -0500

    Added all variables targetting the times by their ID's and giving them functions for past, present and future to change the color on the webpage

--- day-planner.js
+++ day-planner.js
@@ -5,7 +5,18 @@ var currentHour = time.getHours();
 const currentDate = time.getMonth() + 1 + "/" + time.getDate() + "/" + time.getFullYear();
 const currentTime = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
 var nineAM = $("#nineAM");
-
+var tenAM = $("#tenAM")
+var elevAM = $("#elevAM")
+var twelvPM = $("#twelvPM")
+var onePM = $("#onePM")
+var twoPM = $("#twoPM")
+var threePM = $("#threePM")
+var fourPM = $("#fourPM")
+var fivePM = $("#fivePM")
+var sixPM = $("#sixPM")
+var sevPM = $("#sevPM")
+var eightPM = $("#eightPM")
+var ninePM = $("#ninePM")
 // displays current date on webpage
 $("#currentDay").append(currentDate);
 $("#timeOfDay").append(currentTime);
@@ -17,5 +28,98 @@ if (currentHour === 9) {
 } else if (currentHour < 9) {
     nineAM.addClass("futurehour");
 };
-
-
+// 
+if (currentHour === 10) {
+    tenAM.addClass("currenthour")
+} else if (currentHour > 10) {
+    tenAM.addClass("pasthour");
+} else if (currentHour < 10) {
+    tenAM.addClass("futurehour");
+};
+// 
+if (currentHour === 11) {
+    elevAM.addClass("currenthour")
+} else if (currentHour > 11) {
+    elevAM.addClass("pasthour");
+} else if (currentHour < 11) {
+    elevAM.addClass("futurehour");
+};
+// 
+if (currentHour === 12) {
+    twelvPM.addClass("currenthour")
+} else if (currentHour > 12) {
+    twelvPM.addClass("pasthour");
+} else if (currentHour < 12) {
+    twelvPM.addClass("futurehour");
+};
+// 
+if (currentHour === 13) {
+    onePM.addClass("currenthour")
+} else if (currentHour > 13) {
+    onePM.addClass("pasthour");
+} else if (currentHour < 13) {
+    onePM.addClass("futurehour");
+};
+// 
+if (currentHour === 14) {
+    twoPM.addClass("currenthour")
+} else if (currentHour > 14) {
+    twoPM.addClass("pasthour");
+} else if (currentHour < 14) {
+    twoPM.addClass("futurehour");
+};
+// 
+if (currentHour === 15) {
+    threePM.addClass("currenthour")
+} else if (currentHour > 15) {
+    threePM.addClass("pasthour");
+} else if (currentHour < 15) {
+    threePM.addClass("futurehour");
+};
+// 
+if (currentHour === 16) {
+    fourPM.addClass("currenthour")
+} else if (currentHour > 16) {
+    fourPM.addClass("pasthour");
+} else if (currentHour < 16) {
+    fourPM.addClass("futurehour");
+};
+if (currentHour === 17) {
+    fivePM.addClass("currenthour")
+} else if (currentHour > 17) {
+    fivePM.addClass("pasthour");
+} else if (currentHour < 17) {
+    fivePM.addClass("futurehour");
+};
+// 
+if (currentHour === 18) {
+    sixPM.addClass("currenthour")
+} else if (currentHour > 18) {
+    sixPM.addClass("pasthour");
+} else if (currentHour < 18) {
+    sixPM.addClass("futurehour");
+};
+// 
+if (currentHour === 19) {
+    sevPM.addClass("currenthour")
+} else if (currentHour > 19) {
+    sevPM.addClass("pasthour");
+} else if (currentHour < 19) {
+    sevPM.addClass("futurehour");
+};
+// 
+if (currentHour === 20) {
+    nineAM.addClass("currenthour")
+} else if (currentHour > 20) {
+    eightPM.addClass("pasthour");
+} else if (currentHour < 20) {
+    eightPM.addClass("futurehour");
+};
+// 
+if (currentHour === 21) {
+    ninePM.addClass("currenthour")
+} else if (currentHour > 21) {
+    ninePM.addClass("pasthour");
+} else if (currentHour < 21) {
+    ninePM.addClass("futurehour");
+};
